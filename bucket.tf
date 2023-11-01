04d769ab4a387a10c582733dae468de6c7eda2e2
bouranta <kostas.bourantanis@nokia.com>
2023-11-01 16:53:07 +0200

    change bucket.tf to add some file in bucket

--- bucket.tf
+++ bucket.tf
@@ -1,5 +1,5 @@
 provider "aws" {
-    access_key = ${var.aws_access_key}
+    access_key = "${var.aws_access_key}"
     secret_key = "${var.aws_secret_key}"
     region = "${var.region}"
 }
@@ -8,8 +8,16 @@ module "s3" {
     source = "./s3_bucket_dir"
 }
 
-resource "aws_s3_bucket" "temps3examplelolololol" {
+resource "aws_s3_bucket" "assignment_s3_bucket" {
     bucket = "${var.bucket_name}" 
     acl = "${var.acl_value}"
     force_destroy = true
+}
+
+resource "aws_s3_bucket_object" "file_adder" {
+    for_each = fileset("./s3_bucket_dir/", "*")
+    bucket = aws_s3_bucket.assignment_s3_bucket.id
+    key = each.value
+    source = "./s3_bucket_dir/${each.value}"
+    etag = filemd5("./s3_bucket_dir/${each.value}")
 }
